897c6bafefc5106e23d2d72b3a03548ededd9b2c
cse18107 <cse18107@cemk.ac.in>
2021-12-30 13:15:08 +0530

    working on add book page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {Route,Routes} from "react-router-dom";
 import Home from './components/Pages/Home/Home'
 import AddBook from './components/Pages/AddBook/AddBook';
 import EditBook from './components/Pages/EditBook/EditBook';
+import Detail from './components/Pages/Detail/Detail';
 function App() {
   return (
     <>
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Home/>}/>
         <Route path="/add-book" exact element={<AddBook/>}/>
-        <Route path="/edit-book" exact element={<EditBook/>}/>
+        <Route path="/edit-book/:id" exact element={<EditBook/>}/>
+        <Route path="/detail-book/:id" exact element={<Detail/>}/>
       </Routes>
     </>
   );
--- client/src/components/Pages/AddBook/AddBook.js
+++ client/src/components/Pages/AddBook/AddBook.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import "./AddBook.css";
+import logo from "../../../image/books-stack-of-three (1).png";
 
 function AddBook() {
-    return (
-        <div>
-            AddBook
+  return (
+    <div className="addbook-body">
+      <div className="addbook-container">
+        <div className="addbook-content">
+          <img src={logo} alt="book" />
+          <div className="addbook-content__inputs">
+            <div className="title column">
+              <label>Title</label>
+              <input></input>
+            </div>
+            <div className="author column">
+              <label>Author</label>
+              <input></input>
+            </div>
+
+            <div className="category-price">
+              <div className="category">
+                <label>Category</label>
+                <select>
+                  <option>Chemistry</option>
+                  <option>Math</option>
+                  <option>Physics</option>
+                </select>
+              </div>
+              <div className="price">
+                <label>Price</label>
+                <input type="number"></input>
+              </div>
+            </div>
+            <div className="description column">
+              <label>Description</label>
+              <textarea></textarea>
+            </div>
+
+            <div className="buttons">
+              <button className="submit">Submit</button>
+              <button className="cancel">Cancel</button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default AddBook
+export default AddBook;
